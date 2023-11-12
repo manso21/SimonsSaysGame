9c6de0f16b072b9adcceb6fb7ac6edea02fdc7f9
manso21 <alimanso234@gmail.com>
2023-11-12 04:17:27 +0200

    adding comments

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
+// Select all buttons with the class "btn"
 const colorButtons = document.querySelectorAll(".btn");
+
+// Select the element with the ID "level-title"
 const levelTitle = document.getElementById("level-title");
 
+// Game state and logic encapsulated in an object
 const simonGame = {
   isGameOver: true,
   currentLevel: 1,
@@ -8,6 +12,7 @@ const simonGame = {
   currentIndex: 0,
 };
 
+// Event listeners for color buttons
 colorButtons.forEach((button) => {
   button.addEventListener("click", function () {
     animateButton.call(this, this.id + "Audio");
@@ -15,12 +20,14 @@ colorButtons.forEach((button) => {
   });
 });
 
+// Event listener for starting the game on key press
 document.addEventListener("keydown", function () {
   if (simonGame.isGameOver) {
     startGame();
   }
 });
 
+// Function to animate a button
 function animateButton(audio) {
   if (simonGame.isGameOver) return;
   this.classList.add("pressed");
@@ -30,6 +37,7 @@ function animateButton(audio) {
   }, 50);
 }
 
+// Function to start the game
 function startGame() {
   simonGame.colorsOrder = [];
   simonGame.isGameOver = false;
@@ -39,6 +47,7 @@ function startGame() {
   generateRandomColor();
 }
 
+// Function to handle button click
 function handleButtonClick() {
   if (simonGame.isGameOver) return;
   if (this.id !== simonGame.colorsOrder[simonGame.currentIndex]) {
@@ -50,6 +59,7 @@ function handleButtonClick() {
   }
 }
 
+// Function to end the game
 function endGame() {
   simonGame.isGameOver = true;
   updateTitle("Game Over, Press Any Key to Restart");
@@ -60,6 +70,7 @@ function endGame() {
   }, 200);
 }
 
+// Function to advance to the next level
 function advanceLevel() {
   simonGame.currentLevel++;
   updateTitle("Level " + simonGame.currentLevel);
@@ -69,6 +80,7 @@ function advanceLevel() {
   }, 1000);
 }
 
+// Function to generate a random color
 function generateRandomColor() {
   const randomColorButton =
     colorButtons[Math.floor(Math.random() * colorButtons.length)];
@@ -76,10 +88,12 @@ function generateRandomColor() {
   animateButton.call(randomColorButton, randomColorButton.id + "Audio");
 }
 
+// Function to update the title text
 function updateTitle(text) {
   levelTitle.textContent = text;
 }
 
+// Function to play a sound
 function playSound(audio) {
   document.getElementById(audio).play();
 }
